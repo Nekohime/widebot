163d8932669a4261e2f4368edec8a67d2ea05ea8
Nekohime <Nekohime@users.noreply.github.com>
2023-10-13 13:14:09 -0400

    Bot has inworld presence and follows first user in the list.

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ const data = await response.json();
 
 const client = new WsClient(`${wsURL}/api`, encodeURIComponent(data.token));
 const chat = await client.worldChatConnect(1);
+const states = await client.worldStateConnect(1); // get users' states
+
 let message = null;
 let closed = false;
 
@@ -36,5 +38,37 @@ chat.onMessage((msg) => {
 
 chat.onClose((event) => {
   closed = true;
+});
+
+const updateType = {
+  unknown: 0,
+  joining: 1,
+  leaving: 2,
+  moving: 3,
+  teleporting: 4,
+};
+const localUserState = {
+      entityType: entityType.user,
+      updateType: updateType.moving,
+      entityId: 3,
+      x: 2,
+      y: 1,
+      z: 2,
+      yaw: 0,
+      pitch: 0,
+      roll: 0,
+      dataBlock0: 3,
+    };
+
+// states.onMessage((msg) => { console.log(msg); }); //display user srares in terminal
+states.onMessage((msg) => {
+  const u = msg[0];
+  if (u.entityId === 1) {
+    localUserState.x = u.x;
+    localUserState.y = u.y;
+    localUserState.z = u.z;
+  }
+
+  states.send(localUserState)
 });
 // const ws = new WebSocket(wsURL + '/api/worlds/1/ws/state?token=' + encodeURIComponent(data.token));
