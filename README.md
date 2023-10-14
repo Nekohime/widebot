92a29837228f65c99b374abb27715be367790e8f
Nekohime <Nekohime@users.noreply.github.com>
2023-10-13 21:49:10 -0400

    Async, multi-world connection

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # widebot
 A (simple) bot for WideWorlds
 
-Contains files from [WideWorlds](https://github.com/Blaxar/WideWorlds) (common/*, ws-client.js) [MIT](https://github.com/Blaxar/WideWorlds/blob/dev/LICENSE)-licensed
+Contains files from [WideWorlds](https://github.com/Blaxar/WideWorlds) (common/*, ws-client.js) that are [MIT](https://github.com/Blaxar/WideWorlds/blob/dev/LICENSE)-licensed
 
 * npm i
 * npm run bot
--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// import WebSocket from 'isomorphic-ws';
 import fetch from 'node-fetch';
-import {
+/* import {
   serializeEntityState, packEntityStates,
   deserializeEntityState, entityType,
-} from './common/ws-data-format.js';
-// import * as assert from 'assert';
+} from './common/ws-data-format.js'; */
 import WsClient from './ws-client.js';
 
 const authJSON = {username: 'user#3', password: 'user#3'};
@@ -20,26 +18,46 @@ const response = await fetch(httpURL + '/api/login', {
 
 const data = await response.json();
 
+// -------- //
 
-const client = new WsClient(`${wsURL}/api`, encodeURIComponent(data.token));
-const chat = await client.worldChatConnect(1);
-const states = await client.worldStateConnect(1); // get users' states
+// TODO: Modularize
 
-let message = null;
-let closed = false;
 
-chat.onMessage((msg) => {
-  message = JSON.parse(msg);
-  console.log(message.name, message.msg);
-  if (message.msg.startsWith('hi')) {
-    chat.send('Hello! I am a bot! Beep boop!');
-  }
-});
+// World ids to join
+const worlds = [
+  1, 2,
+];
 
-chat.onClose((event) => {
-  closed = true;
-});
+// Process each world in parallel, in any order.
+await Promise.all(worlds.map(async (world) => {
+  await handleWorld(world);
+}));
 
+// Magic happens here
+async function handleWorld(world) {
+  const client = new WsClient(`${wsURL}/api`, encodeURIComponent(data.token));
+  const chat = await client.worldChatConnect(world);
+  // TODO: multiworld state stuff
+  // const states = await client.worldStateConnect(1); // get users' states
+
+  let message = null;
+  // let closed = false;
+
+  chat.onMessage((m) => {
+    message = JSON.parse(m);
+
+    console.log(`[World#${world}] <${message.name}>: ${message.msg}`);
+    if (message.msg.startsWith('hi')) {
+      chat.send('Hello! I am a bot! Beep boop!');
+    }
+  });
+
+  chat.onClose((event) => {
+    // closed = true;
+  });
+}
+
+/*
 const updateType = {
   unknown: 0,
   joining: 1,
@@ -48,19 +66,18 @@ const updateType = {
   teleporting: 4,
 };
 const localUserState = {
-      entityType: entityType.user,
-      updateType: updateType.moving,
-      entityId: 3,
-      x: 2,
-      y: 1,
-      z: 2,
-      yaw: 0,
-      pitch: 0,
-      roll: 0,
-      dataBlock0: 3,
-    };
+  entityType: entityType.user,
+  updateType: updateType.moving,
+  entityId: 3,
+  x: 2,
+  y: 1,
+  z: 2,
+  yaw: 0,
+  pitch: 0,
+  roll: 0,
+  dataBlock0: 3,
+};
 
-// states.onMessage((msg) => { console.log(msg); }); //display user srares in terminal
 states.onMessage((msg) => {
   const u = msg[0];
   if (u.entityId === 1) {
@@ -69,6 +86,6 @@ states.onMessage((msg) => {
     localUserState.z = u.z;
   }
 
-  states.send(localUserState)
+  states.send(localUserState);
 });
-// const ws = new WebSocket(wsURL + '/api/worlds/1/ws/state?token=' + encodeURIComponent(data.token));
+*/
